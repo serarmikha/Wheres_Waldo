b653035faf0e7fafba2b3c779e62c16dd69ad068
Serar Mikha <serar.m@gmail.com>
2016-06-01 22:22:49 -0700

    Cleaned Up Code: -Removed tracking bars for image filter manipulation -Only displaying the results picture, with a larger rectangle around Waldo

--- Wheres_Waldo/main.cpp
+++ Wheres_Waldo/main.cpp
@@ -6,50 +6,7 @@
 
 using namespace cv;
 
-const std::string TRACKBARWINDOWNAME = "Trackbars";
 
-//changed values for trackbars
-int H_MIN = 160;
-int H_MAX = 179;
-int S_MIN = 100;
-int S_MAX = 255;
-int V_MIN = 100;
-int V_MAX = 255;
-
-void on_trackbar(int, void*)
-{//This function gets called whenever a
- // trackbar position is changed
-}
-
-//from: https://raw.githubusercontent.com/kylehounslow/opencv-tuts/master/object-tracking-tut/objectTrackingTut.cpp
-void createTrackbars() 
-{
-	//create window for trackbars
-
-
-	namedWindow(TRACKBARWINDOWNAME, 0);
-	//create memory to store trackbar name on window
-	char TrackbarName[50];
-	sprintf(TrackbarName, "H_MIN", H_MIN);
-	sprintf(TrackbarName, "H_MAX", H_MAX);
-	sprintf(TrackbarName, "S_MIN", S_MIN);
-	sprintf(TrackbarName, "S_MAX", S_MAX);
-	sprintf(TrackbarName, "V_MIN", V_MIN);
-	sprintf(TrackbarName, "V_MAX", V_MAX);
-	//create trackbars and insert them into window
-	//3 parameters are: the address of the variable that is changing when the trackbar is moved(eg.H_LOW),
-	//the max value the trackbar can move (eg. H_HIGH), 
-	//and the function that is called whenever the trackbar is moved(eg. on_trackbar)
-	//                                  ---->    ---->     ---->      
-	createTrackbar("H_MIN", TRACKBARWINDOWNAME, &H_MIN, H_MAX, on_trackbar);
-	createTrackbar("H_MAX", TRACKBARWINDOWNAME, &H_MAX, H_MAX, on_trackbar);
-	createTrackbar("S_MIN", TRACKBARWINDOWNAME, &S_MIN, S_MAX, on_trackbar);
-	createTrackbar("S_MAX", TRACKBARWINDOWNAME, &S_MAX, S_MAX, on_trackbar);
-	createTrackbar("V_MIN", TRACKBARWINDOWNAME, &V_MIN, V_MAX, on_trackbar);
-	createTrackbar("V_MAX", TRACKBARWINDOWNAME, &V_MAX, V_MAX, on_trackbar);
-
-
-}
 
 int main(void)
 {
@@ -58,42 +15,41 @@ int main(void)
 	Mat imgCopy;		//copy of image
 	Mat	imgFiltered;	//filtered image
 	Mat imgTemplate;			//template image
-	Mat imgTemplateFiltered;	//filtered template
+	Mat imgTemplateFiltered;	//filtered templat
 
-	createTrackbars();
-	//while (1)
-	//{
-		//open image
-		imgOriginal = imread("waldo.jpg");
-		imgTemplate = imread("template.jpg");
+	//Values for image filtering 
+	int H_MIN = 160;
+	int H_MAX = 179;
+	int S_MIN = 100;
+	int S_MAX = 255;
+	int V_MIN = 100;
+	int V_MAX = 255;
 
-		if (imgOriginal.empty())
-		{
-			std::cout << "ERROR: Image not found" << std::endl;
+	//open image
+	imgOriginal = imread("waldo.jpg");
+	imgTemplate = imread("template.jpg");
 
-		}
+	if (imgOriginal.empty())
+	{
+		std::cout << "ERROR: Image not found" << std::endl;
+
+	}
 	
-		if (imgTemplate.empty())
-		{
-			std::cout << "ERROR: Template not found" << std::endl;
+	if (imgTemplate.empty())
+	{
+		std::cout << "ERROR: Template not found" << std::endl;
 
-		}
+	}
 
-		cvtColor(imgOriginal, imgCopy, CV_BGR2HSV);
-		cvtColor(imgTemplate, imgTemplateFiltered, CV_BGR2HSV);
+	//Convert image to HSV for easier filtering
+	cvtColor(imgOriginal, imgCopy, CV_BGR2HSV);
+	cvtColor(imgTemplate, imgTemplateFiltered, CV_BGR2HSV);
 
-		inRange(imgCopy, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), imgFiltered);
-		inRange(imgTemplateFiltered, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), imgTemplateFiltered);
+	//filter out anything that is not red on both images
+	inRange(imgCopy, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), imgFiltered);
+	inRange(imgTemplateFiltered, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), imgTemplateFiltered);
 
-		/*namedWindow("imgOriginal", CV_WINDOW_NORMAL);
-		namedWindow("imgFiltered", CV_WINDOW_NORMAL);
-
-		imshow("imgOriginal", imgOriginal);
-		imshow("imgFiltered", imgFiltered);*/
-
-	//	waitKey(30);
-//	}
-
+	//create a matrix to hold the results of the matching template
 	int cols = imgFiltered.cols - imgTemplateFiltered.cols - 1;
 	int rows = imgFiltered.rows - imgTemplateFiltered.rows - 1;
 
@@ -102,16 +58,17 @@ int main(void)
 	Mat result;
 	result.create(rows, cols, CV_32FC1);	
 
+	//match template to "Where's Waldo" image
 	matchTemplate(imgFiltered, imgTemplateFiltered, result, CV_TM_SQDIFF);
 	normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
 	minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+
 	matchedLocation = minLoc;
 
-	rectangle(imgOriginal, matchedLocation, Point(matchedLocation.x + imgTemplateFiltered.cols, matchedLocation.y + imgTemplateFiltered.rows), Scalar::all(0), 2, 8, 0);
-	rectangle(result, matchedLocation, Point(matchedLocation.x + imgTemplateFiltered.cols, matchedLocation.y + imgTemplateFiltered.rows), Scalar::all(255), 2, 8, 0);
+	//show the results
+	rectangle(imgOriginal, matchedLocation, Point(matchedLocation.x + imgTemplateFiltered.cols+30, matchedLocation.y + imgTemplateFiltered.rows+30), Scalar::all(0), 2, 8, 0);
 
 	imshow("original", imgOriginal);
-	imshow("result", result);
 
 	waitKey(0);
 	return 0;
